e12cd66d0c892ba9dadb2ce38c040cd233f48df9
DrBracewell <doctorbracewell@gmail.com>
2021-10-29 21:42:11 +0100

    v2!

--- index.js
+++ index.js
@@ -4,15 +4,39 @@ document.querySelector("form").addEventListener("submit", async (event) => {
   event.preventDefault();
 
   try {
-    const fileReader = new FileReader();
-    fileReader.readAsDataURL(document.querySelector("#skin").files[0]);
+    if (document.querySelector("#skin").disabled) {
+      const username = document.querySelector("#username").value;
 
-    fileReader.addEventListener("load", (e) => {
-      document.querySelector(".progress").style.display = "flex";
-      createSkins(e.target.result);
-    });
+      const {
+        textures: {
+          skin: { data },
+        },
+      } = await fetch(`https://api.ashcon.app/mojang/v2/user/${username}`).then(
+        (res) => res.json()
+      );
+
+      createSkins(`data:image/png;base64,${data}`);
+    } else {
+      const fileReader = new FileReader();
+      fileReader.readAsDataURL(document.querySelector("#skin").files[0]);
+
+      fileReader.addEventListener("load", (e) => {
+        createSkins(e.target.result);
+      });
+    }
   } catch (error) {
     document.querySelector(".progress").style.display = "none";
     alert("Something went wrong! Reload to try again\n" + error);
   }
+});
+
+document.querySelector("#username").addEventListener("input", (event) => {
+  if (event.target.value === "")
+    return (document.querySelector("#skin").disabled = false);
+
+  document.querySelector("#skin").disabled = true;
+});
+
+document.querySelector("#skin").addEventListener("change", () => {
+  document.querySelector("#username").disabled = true;
 });
--- pillowGenerator.js
+++ pillowGenerator.js
@@ -1,5 +1,6 @@
-function createCubeArray({ body, leftArm, rightArm, head }) {
-  console.log(body, leftArm, rightArm, head);
+function createCubeArray({ body, leftArm, rightArm, head }, size) {
+  const headSize = size === "normal" ? 0.25 : 0.59;
+
   // Represent the pillow as a 3D grid, where each cube is 0.25 units length/width/height
   // Each cube also has its skin data associated with it - ID and texture value
 
@@ -12,8 +13,8 @@ function createCubeArray({ body, leftArm, rightArm, head }) {
   for (let i = 0; i < 2; i++) {
     for (let j = 0; j < 6; j++) {
       cubeArray.push({
-        x: 0.25 + 0.25 * i, // for each Y value, we want one cube at 0.25 and one at 0.5
-        y: 0.25 * j, // One cube for 6 increases of 0.25
+        x: headSize + headSize * i, // for each Y value, we want one cube at 0.25 and one at 0.5
+        y: headSize * j, // One cube for 6 increases of 0.25
         z: 0.375, //Z is always (1-0.25)/2 = 0.375 since the cube must be directly in the middle, so it must start with an offset of 0.375.
         skin: body[j + i * 6],
       });
@@ -24,7 +25,7 @@ function createCubeArray({ body, leftArm, rightArm, head }) {
   for (let i = 0; i < 3; i++) {
     cubeArray.push({
       x: 0,
-      y: 0.75 + i * 0.25,
+      y: 3 * headSize + i * headSize,
       z: 0.375,
       skin: leftArm[i],
     });
@@ -32,8 +33,8 @@ function createCubeArray({ body, leftArm, rightArm, head }) {
   // 3x1*1 Arm 2
   for (let i = 0; i < 3; i++) {
     cubeArray.push({
-      x: 0.75,
-      y: 0.75 + i * 0.25,
+      x: 3 * headSize,
+      y: 3 * headSize + i * headSize,
       z: 0.375,
       skin: rightArm[i],
     });
@@ -46,9 +47,9 @@ function createCubeArray({ body, leftArm, rightArm, head }) {
     for (let j = 0; j < 2; j++) {
       for (let k = 0; k < 2; k++) {
         cubeArray.push({
-          x: 0.25 + i * 0.25,
-          y: 1.5 + j * 0.25,
-          z: 0.25 + k * 0.25,
+          x: headSize + i * headSize,
+          y: 6 * headSize + j * headSize,
+          z: (size === "normal" ? headSize : 0.08) + k * headSize,
           skin: head[indexTracker],
         });
         indexTracker++;
@@ -56,12 +57,25 @@ function createCubeArray({ body, leftArm, rightArm, head }) {
     }
   }
 
-  // Finally add a command to summon a named armor stand
-  if (document.querySelector("#name").value)
-    cubeArray.push({
+  const nameStandPos = {
+    normal: {
       x: -0.45,
       y: 0.8,
       z: 0.8,
+    },
+    giant: {
+      x: 0.875,
+      y: 4.125,
+      z: 0.4,
+    },
+  };
+
+  // Finally add a command to summon a named armor stand
+  if (document.querySelector("#name").value)
+    cubeArray.push({
+      x: nameStandPos[size].x,
+      y: nameStandPos[size].y,
+      z: nameStandPos[size].z,
       name: document.querySelector("#name").value + "&r",
     });
 
@@ -70,42 +84,64 @@ function createCubeArray({ body, leftArm, rightArm, head }) {
 }
 
 // Recursive function to create the riding command
-function walkArray(arr, fullArr) {
+function walkArray(arr, fullArr, size) {
   if (arr.length === 0) return "";
 
   return (
     `Riding:{id:\"FallingSand\",Block:command_block,Data:0,TileEntityData:{Command:\"${calculateSummonCommand(
       arr[0],
-      fullArr
+      fullArr,
+      size
     )}\"},Time:1,DropItem:0,` +
-    walkArray(arr.slice(1), fullArr) +
+    walkArray(arr.slice(1), fullArr, size) +
     "}"
   );
 }
 
 // Use the co-ordinates to calculate the offset the stand needs to be summoned at
-function calculateSummonCommand(cubeData, cubeArray) {
+function calculateSummonCommand(cubeData, cubeArray, size) {
+  const normalSized = size === "normal";
   const index = cubeArray.indexOf(cubeData);
+
+  const sizes = {
+    normal: {
+      x: -0.05,
+      y: -0.9,
+      z: -0.3,
+    },
+    giant: {
+      x: 0,
+      y: -1.425,
+      z: 0,
+    },
+  };
 
   // Remove 0.5 blocks from each cord to account for the CMB summoning it in centre
   // And remove other amount from the offset of the armor stand holding it in a specific position
-  const x = cubeData.x + 0.5 - 0.05;
-  const y = (cubeArray.length - index - cubeData.y) * -1 - 0.5 - 0.9;
-  const z = cubeData.z - 0.5 - 0.3;
+  const x = cubeData.x + 0.5 - 0.05 + sizes[size].x;
+  const y = (cubeArray.length - index - cubeData.y) * -1 - 0.5 + sizes[size].y;
+  const z = cubeData.z - 0.5 + sizes[size].z;
 
   return `/summon ArmorStand ~${x} ~${y} ~${z} {${
     cubeData?.name
       ? `CustomName:\\"${cubeData.name}\\",CustomNameVisible:1b,`
       : ""
-  }NoGravity:1b,Invisible:1b,Invulnerable:1b,Pose:{RightArm:[-45.0f,45.0f,0.0f]},ShowArms:1b${
+  }NoGravity:1b,Rotation:[180f],Invisible:1b,Invulnerable:1b,Pose:{RightArm:[-45.0f,45.0f,0.0f]},ShowArms:1b${
     cubeData?.skin?.id
-      ? `,Equipment:[{id:\\"skull\\",Count:1b,Damage:3s,tag:{SkullOwner:{Id:\\"${cubeData.skin.id}\\",Properties:{textures:[{Value:\\"${cubeData.skin.value}\\"}]}}}},{},{},{},{}]`
+      ? `,Equipment:[${
+          normalSized ? "" : "{},{},{},{},"
+        }{id:\\"skull\\",Count:1b,Damage:3s,tag:{SkullOwner:{Id:\\"${
+          cubeData.skin.id
+        }\\",Properties:{textures:[{Value:\\"${cubeData.skin.value}\\"}]}}}}${
+          normalSized ? ",{},{},{},{}" : ""
+        }]`
       : ""
   }}`;
 }
 
 export function generateCommand(skin) {
-  const cubeArray = createCubeArray(skin);
+  const size = document.querySelector("input[name='size']:checked").value;
+  const cubeArray = createCubeArray(skin, size);
 
   // Setup intial command
   const initialCommand = `/summon FallingSand ~ ~1 ~ {Block:redstone_block,Data:0,Time:1,DropItem:0,Riding:{id:\"FallingSand\",Block:command_block,Data:0,TileEntityData:{Command:\"/fill ~1 ~-1 ~ ~1 ~-${
@@ -116,5 +152,5 @@ export function generateCommand(skin) {
 
   document.querySelector(".progress").style.display = "none";
   document.querySelector("#output").textContent =
-    initialCommand + walkArray(cubeArray, cubeArray) + "}}}";
+    initialCommand + walkArray(cubeArray, cubeArray, size) + "}}}";
 }
