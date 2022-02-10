cf62fc6a84c17dc85b434320754f50fa6af5223b
DrBracewell <doctorbracewell@gmail.com>
2022-02-10 16:21:20 +0000

    Create nbt.js

---
+++ src/pillow/nbt.js
@@ -0,0 +1,45 @@
+// Parser for abstract syntax tree to generate Minecraft nbt data string
+/* Example Input:
+[{
+  type: "node"
+  key: "Block",
+  value: "redstone_block"
+},
+{
+  type: "tree"
+  key: "Riding",
+  value: [{
+    type: "node",
+    key: "Block",
+    value: "command_block"
+  },{
+    type: "array",
+    key: "textures",
+    value: [{
+      type: "node",
+      key: "Value",
+    },
+  {
+    type: "value",
+    value: 1
+  }]
+}]
+*/
+
+export function generateNbtString(nbtData) {
+  return nbtData.reduce((acc, node, index) => {
+    const endingString = index === nbtData.length - 1 ? "" : ",";
+
+    if (node.type === "value") return acc + node.value + endingString;
+    if (node.type === "node")
+      return acc + `${node.key}:${node.value}` + endingString;
+    if (node.type === "tree")
+      return (
+        acc + `${node.key}:{${generateNbtString(node.value)}}` + endingString
+      );
+    if (node.type === "array")
+      return (
+        acc + `${node.key}:[${generateNbtString(node.value)}]` + endingString
+      );
+  }, "");
+}
